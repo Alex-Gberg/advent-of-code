453a7e7582289ba6482592e6913c1ae6797100e9
Alex-Gberg <alexandergoldberg97@gmail.com>
2023-12-06 07:37:41 +0100

    Day 6: Part 2 optimization

--- day6/python/d6p2.py
+++ day6/python/d6p2.py
@@ -1,8 +1,24 @@
 time = int(''.join(input().strip().split(":")[1].strip().split()))
 distance = int(''.join(input().strip().split(":")[1].strip().split()))
 
-waysToWin = 0
-for i in range(1,time+1):
-    if (time-i)*i > distance:
-        waysToWin += 1
-print(waysToWin)
+l1 = 1
+r1 = time
+while l1<=r1:
+    guess = l1+(r1-l1)//2
+    if (time-guess)*guess > distance:
+        r1 = guess-1
+        continue
+    if (time-guess)*guess <= distance:
+        l1 = guess+1
+
+l2 = 1
+r2 = time
+while l2<=r2:
+    guess = l2+(r2-l2)//2
+    if (time-guess)*guess > distance:
+        l2 = guess+1
+        continue
+    if (time-guess)*guess <= distance:
+        r2 = guess-1
+
+print(r2-r1)

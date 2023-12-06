1ff86c58b0547f76e8db5b029a28c63c7c43dd64
Alex-Gberg <alexandergoldberg97@gmail.com>
2023-12-06 06:58:43 +0100

    Day 5: Part 2 solve

--- day5/python/d5p2.py
+++ day5/python/d5p2.py
@@ -1,5 +1,5 @@
 seedInfo = [int(x) for x in input().strip().split(':')[1].strip().split()]
-seedRanges = [(seedInfo[i], seedInfo[i]+seedInfo[i+1]-1) for i in range(0, len(seedInfo),2)]
+seedRanges = [(seedInfo[i], seedInfo[i]+seedInfo[i+1]-1) for i in range(0, len(seedInfo), 2)]
 seedRanges.sort(key=lambda x: x[0])
 input()
 
@@ -23,30 +23,46 @@ while True:
     map.sort(key=lambda x: x[0])
     maps.append(map)
 
-locations = []
-mapPointer = 0
-for r in seedRanges:
-    rL = r[0]
-    rR = r[1]
+
+ranges = seedRanges
+for stage in maps:
     newRanges = []
-    for i in range(mapPointer,):
-        if rR < maps[i][0]:
-            newRanges.append((rL, rR))
-            mapPointer = i
-            break
-        if rL > maps[i][1]:
-            continue
-        if maps[i][0] <= rL and rR <= maps[i][1]:
-            newRanges.append((rL+maps[i][2], rR+maps[i][2]))
-            mapPointer = i
-            break
-        if rL <= maps[i][1]:
-            
-
-
-
-
-
-        mapPointer = i
-
-print(min(locations))
+    mapPointer = 0
+    for r in ranges:
+        rL = r[0]
+        rR = r[1]
+        for i in range(mapPointer, len(stage)):
+            if rL > rR:
+                break
+            if rR < stage[i][0]:
+                newRanges.append((rL, rR))
+                mapPointer = i
+                break
+            if rL > stage[i][1]:
+                if i == len(stage)-1:
+                    newRanges.append((rL, rR))
+                    mapPointer = i
+                    break
+                continue
+            if rL < stage[i][0]:
+                newRanges.append((rL, stage[i][0]-1))
+                if rR <= stage[i][1]:
+                    newRanges.append((stage[i][0]+stage[i][2], rR+stage[i][2]))
+                    mapPointer = i
+                    break
+                else:
+                    newRanges.append((stage[i][0]+stage[i][2], stage[i][1]+stage[i][2]))
+                    rL = stage[i][1]+1
+                    continue
+            if rL >= stage[i][0]:
+                if rR <= stage[i][1]:
+                    newRanges.append((rL+stage[i][2], rR+stage[i][2]))
+                    mapPointer = i
+                    break
+                else:
+                    newRanges.append((rL+stage[i][2], stage[i][1]+stage[i][2]))
+                    rL = stage[i][1]+1
+                    continue
+    newRanges.sort(key=lambda x: x[0])
+    ranges = newRanges
+print(ranges[0][0])

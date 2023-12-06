69985ffdaa980c8d61ecfdbc406b826a95f537d0
Alex-Gberg <alexandergoldberg97@gmail.com>
2023-12-06 22:56:29 +0100

    Day 6: Constant time solution

--- day6/python/d6p1.py
+++ day6/python/d6p1.py
@@ -1,11 +1,10 @@
+from math import sqrt, floor, ceil
+
 times = [int(x) for x in input().strip().split(":")[1].strip().split()]
 distances = [int(x) for x in input().strip().split(":")[1].strip().split()]
 
 total = 1
-for t, d in zip(times, distances):
-    waysToWin = 0
-    for i in range(1,t+1):
-        if (t-i)*i > d:
-            waysToWin += 1
-    total *= waysToWin
+for time, distance in zip(times, distances):
+    det = sqrt(time**2-4*distance)
+    total *= ceil(((-time - det)/-2)-1) - floor(((-time + det)/-2)+1) + 1
 print(total)
--- day6/python/d6p2.py
+++ day6/python/d6p2.py
@@ -1,24 +1,6 @@
+from math import sqrt, ceil, floor
+
 time = int(''.join(input().strip().split(":")[1].strip().split()))
 distance = int(''.join(input().strip().split(":")[1].strip().split()))
 
-l1 = 1
-r1 = time
-while l1<=r1:
-    guess = l1+(r1-l1)//2
-    if (time-guess)*guess > distance:
-        r1 = guess-1
-        continue
-    if (time-guess)*guess <= distance:
-        l1 = guess+1
-
-l2 = 1
-r2 = time
-while l2<=r2:
-    guess = l2+(r2-l2)//2
-    if (time-guess)*guess > distance:
-        l2 = guess+1
-        continue
-    if (time-guess)*guess <= distance:
-        r2 = guess-1
-
-print(r2-r1)
+print(ceil(((-time - sqrt(time**2-4*distance))/-2)-1) - floor(((-time + sqrt(time**2-4*distance))/-2)+1) + 1)

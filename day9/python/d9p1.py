6921377b5af1370f2db06cbfdfcdd20f42b791b8
Alex-Gberg <alexandergoldberg97@gmail.com>
2023-12-09 09:58:36 +0100

    Day 9: Solve

---
+++ day9/python/d9p1.py
@@ -0,0 +1,18 @@
+inp = [int(x) for x in input().strip().split()]
+
+def rec(l):
+    if all([x == 0 for x in l]):
+        return 0
+    
+    return l[-1] + rec([l[i+1]-l[i] for i in range(len(l)-1)])
+
+sum = 0
+while inp:
+    sum += rec(inp)
+
+    try:
+        inp = [int(x) for x in input().strip().split()]
+    except EOFError:
+        break
+
+print(sum)

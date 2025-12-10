ccba1f2439386f2dd028f44b13c1f763a733462a
Alex-Gberg <alexandergoldberg97@gmail.com>
2025-12-10 09:13:35 +0100

    2024 d7

---
+++ 2024/day7/d7p1.py
@@ -0,0 +1,27 @@
+def compute(target, val, operands):
+    if operands == []:
+        return target == val
+    if val > target:
+        return False
+
+    return (
+        compute(target, val + operands[0], operands[1:]) 
+        or compute(target, val * operands[0], operands[1:])
+    )
+
+
+count = 0
+while True:
+    try:
+        equation = input()
+    except EOFError:
+        break
+
+    answer, operands = (
+        int(equation.split(":")[0]),
+        [int(x) for x in equation.split(":")[1].split()],
+    )
+    if compute(answer, operands[0], operands[1:]):
+        count += answer
+
+print(count)

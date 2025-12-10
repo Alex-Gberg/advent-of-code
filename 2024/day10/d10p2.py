fa29aa52703dfc390fffabe39574330cca7f5934
Alex-Gberg <alexandergoldberg97@gmail.com>
2025-12-10 09:13:35 +0100

    2024 d10

---
+++ 2024/day10/d10p2.py
@@ -0,0 +1,41 @@
+from collections import deque
+
+map = []
+trailheads = []
+i = 0
+while True:
+    try:
+        inp = [int(x) if x != '.' else -1 for x in list(input())]
+        for j,x in enumerate(inp):
+            if x == 0:
+                trailheads.append((i,j))
+
+        map.append(inp)
+        i += 1
+    except:
+        break
+
+
+total_score = 0
+for th in trailheads:
+    q = deque()
+    q.append(th)
+    score = 0
+    while len(q) > 0:
+        curr_pos = q.popleft()
+        if map[curr_pos[0]][curr_pos[1]] == 9:
+            score += 1
+
+
+        if curr_pos[0] > 0 and map[curr_pos[0]][curr_pos[1]] + 1 == map[curr_pos[0]-1][curr_pos[1]]:
+                q.append((curr_pos[0]-1, curr_pos[1]))
+        if curr_pos[1] > 0 and map[curr_pos[0]][curr_pos[1]] + 1 == map[curr_pos[0]][curr_pos[1]-1]:
+                q.append((curr_pos[0], curr_pos[1]-1))
+        if curr_pos[0] < len(map)-1 and map[curr_pos[0]][curr_pos[1]] + 1 == map[curr_pos[0]+1][curr_pos[1]]:
+                q.append((curr_pos[0]+1, curr_pos[1]))
+        if curr_pos[1] < len(map[0])-1 and map[curr_pos[0]][curr_pos[1]] + 1 == map[curr_pos[0]][curr_pos[1]+1]:
+                q.append((curr_pos[0], curr_pos[1]+1))
+    total_score += score
+
+
+print(total_score)
